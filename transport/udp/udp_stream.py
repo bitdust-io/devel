5fab95dfd54aee380a2b220ed388b07f08155d58
Veselin <penev.veselin@gmail.com>
2014-07-27 19:47:13 +0400

    27.07.2014 at 19:47:13

--- transport/udp/udp_stream.py
+++ transport/udp/udp_stream.py
@@ -176,7 +176,7 @@ class UDPStream():
                 data_size = len(piece)
                 if time_sent >= 0:
                     dt = relative_time - time_sent 
-                    if dt > RTT_MAX_LIMIT:
+                    if dt > self.last_ack_rtt * 2:
                         self.resend_bytes += data_size
                         # print 're -',
                     else:
@@ -217,10 +217,11 @@ class UDPStream():
             self.resend_inactivity_counter += 1
         next_resend = max(min(self.last_ack_rtt, RTT_MAX_LIMIT), RTT_MIN_LIMIT)
         if self.resend_inactivity_counter > 10:
-            print 'drop resend out:%s acks:%s' % (len(self.output_blocks.keys()), len(self.blocks_to_ack))
-            next_resend = RTT_MAX_LIMIT * 2.0
+            if self.resend_inactivity_counter % 10 == 10:
+                print 'drop resend out:%s acks:%s' % (len(self.output_blocks.keys()), len(self.blocks_to_ack))
+            next_resend = RTT_MAX_LIMIT * 4.0
         if len(self.blocks_to_ack) > 0:
-            if time.time() - self.last_ack_moment > RTT_MAX_LIMIT:
+            if time.time() - self.last_ack_moment > next_resend:
                 self.send_ack()
         if len(self.output_blocks):        
             self.send_blocks()
