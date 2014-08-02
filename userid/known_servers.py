e3dede4632b4f92ee01b51c7affbb14e59f73c25
Veselin <penev.veselin@gmail.com>
2014-08-02 19:58:55 +0400

    02.08.2014 at 19:58:55

--- userid/known_servers.py
+++ userid/known_servers.py
@@ -13,5 +13,6 @@ def by_host():
     Keys are domain names or global IP address of the server.
     """
     return {
-        'megafaq.ru':   (80, 6661),
+        # 'megafaq.ru':   (80, 6661),
+        '37.18.255.32' : (80, 6661),
         }
