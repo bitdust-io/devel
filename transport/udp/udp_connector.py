43ca2dc784ffeb441a6c1ca34df986da52cac371
Veselin <penev.veselin@gmail.com>
2014-08-03 09:03:03 +0400

    03.08.2014 at  9:03:03

--- transport/udp/udp_connector.py
+++ transport/udp/udp_connector.py
@@ -149,7 +149,8 @@ class DHTUDPConnector(automat.Automat):
         automat.objects().pop(self.index)
 
     def _got_peer_incomings(self, value):
-        # lg.out(18, 'udp_connector._got_peer_incomings %r' % value)
+        lg.out(18, 'udp_connector._got_peer_incomings:')
+        lg.out(18, str(value))
         current_incomings = []
         if type(value) == dict:
             try:
