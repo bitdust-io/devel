7fa4c3a21720b62e684b8c01e77beb0f5abadbfc
Veselin <penev.veselin@gmail.com>
2014-08-10 19:59:43 +0400

    10.08.2014 at 19:59:43

--- transport/udp/udp_stream.py
+++ transport/udp/udp_stream.py
@@ -58,7 +58,7 @@ MAX_ACK_TIMEOUTS = 5
 
 _Streams = {}
 _GlobalLimitSendBytesPerSec = 100.0 * 125000
-_Debug = True
+_Debug = False
 
 #------------------------------------------------------------------------------ 
 
