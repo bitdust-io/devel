e2bdb740f58bedd05d6ff2ff9f813d0bfdec968b
Veselin <penev.veselin@gmail.com>
2014-08-24 09:37:00 +0400

    2014-08-24 at  9:37:00

---
+++ release/sources/0-build.bat
@@ -0,0 +1,37 @@
+
+@echo off
+
+
+@echo [ prepare workspace folder ]
+rm -rf workspace
+mkdir workspace
+mkdir workspace\bitpie
+cp MANIFEST.in workspace
+
+@echo [ export from git repo into workspace ]
+pushd ..\.. 
+git archive master > release\sources\workspace\bitpie\master.tar
+popd 
+pushd workspace\bitpie
+tar xf master.tar
+rm master.tar
+popd
+
+@echo [ prepare setup.py file ]
+cp setup.py workspace
+python -c "v=open('../version').read().strip(); s=open('workspace/setup.py').read(); s=s.replace('{version}', v); open('workspace/setup.py', 'w').write(s);"
+
+@echo [ remove some files ]
+pushd workspace\bitpie
+rm commit.bat
+rm commit.sh
+rm -rf screenshots\*.*
+rm -rf release\*.*
+
+@echo [ move some files to the top level ]
+mv -v *.txt ..
+
+popd
+
+@echo [ sources done ]
+pause
