c27864e0716b85ad00df3caf570f3e9982ae1398
Veselin <penev.veselin@gmail.com>
2014-09-17 13:53:11 +0400

    2014-09-17 at 13:53:11

--- lib/misc.py
+++ lib/misc.py
@@ -1367,7 +1367,8 @@ def DoRestart(param='', detach=False):
         if detach:
         #     from lib import child_process
         #     return child_process.detach(cmdargs)
-            return os.spawnv(os.P_DETACH, pypath, cmdargs)
+            # return os.spawnv(os.P_DETACH, pypath, cmdargs)
+            return os.spawnv(os.P_NOWAIT, 'nohup', [pypath,]+cmdargs)
         return os.execvpe(pypyth, cmdargs, os.environ)
             
 def RunBatFile(filename, output_filename=None):
