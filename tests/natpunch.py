5fbe0550d367a295ba9d363d44b502b57a29d457
Veselin <penev.veselin@gmail.com>
2014-10-25 16:03:10 +0400

    modified:   tests/natpunch.py

--- tests/natpunch.py
+++ tests/natpunch.py
@@ -17,58 +17,91 @@ from lib import bpio
 from lib import udp
 from lib import settings
 
-from stun import stun_client
-from dht import dht_service
+# from stun import stun_client
+# from dht import dht_service
 
 #------------------------------------------------------------------------------ 
 
-def loop_send_packets(min_port, max_port):
-    n = 0
-    if max_port - min_port > 100:
-        max_port = min_port + 100
-    for port_num in range(min_port, max_port+1):
-        udp.send_command(int(settings.getUDPPort()), udp.CMD_PING, 'ping', (sys.argv[1], port_num))
-        n += 1
-    print 'sent %d packets to %s' % (n, sys.argv[1])
-    reactor.callLater(10, loop_send_packets, min_port, max_port)
+def listen(local_port, servers, incomings_filename):
+    def _loop():
+        incomings = []
+        for line in open(incomings_filename).read().split('\n'):
+            addr = line.split(':')
+            addr[1] = int(addr[1])
+            incomings.append(addr)
+        if len(incomings):
+            for srv in servers:
+                udp.send_command(local_port, udp.CMD_PING, 'ping', srv)
+            for inc in incomings:
+                udp.send_command(local_port, udp.CMD_PING, 'ping', inc)
+        reactor.callLater(5, _loop)
+    _loop()
 
-def datagram_received(datagram, address):
+def connect(local_port, remote_ip, servers, min_port, max_port):
+    def _loop():
+        for srv in servers:
+            udp.send_command(local_port, udp.CMD_PING, 'ping', srv)
+        for port_num in range(min_port, max_port+1):
+            udp.send_command(local_port, udp.CMD_PING, 'ping', (remote_ip, port_num))
+        reactor.callLater(5, _loop)
+    _loop()
+
+def datagram_received(datagram, address, local_port):
     try:
         cmd, payload = datagram
     except:
         return
-    if cmd != udp.CMD_PING:
-        return
-    print 'datagram [%s] from %r' % (datagram, address)
-    if address[0] == sys.argv[1]:
-        print 'OKAY!!!!!!!!!!!!!!', address
-        udp.send_command(int(settings.getUDPPort()), udp.CMD_PING, 'ok', address)
-        # reactor.stop()
+    # if cmd != udp.CMD_PING:
+        # return
+    print 'datagram [%s] from %r' % (payload, address)
+#    if address[0] == sys.argv[1]:
+#        print 'OKAY!!!!!!!!!!!!!!', address
+#        udp.send_command(local_port, udp.CMD_PING, 'ok', address)
+#        # reactor.stop()
 
 def main():
-    print 'usage:  natpunch.py  <remote IP> [min port] [max port]'
+    if len(sys.argv) <= 1:
+        print 'usage:'
+        print '    natpunch.py server [min port] [max port]'
+        print '    natpunch.py listen [local port] [servers list filename] [incoming connections filename]'
+        print '    natpunch.py connect [local port] [remote IP] [servers list file] [min port] [max port]'
+        return
+
     lg.set_debug_level(24)
     bpio.init()
-    dht_service.init(int(settings.getDHTPort()))
-    udp.listen(int(settings.getUDPPort()))
-    udp.proto(int(settings.getUDPPort())).add_callback(datagram_received)
-    def _finished(x, result):
-        # print 'stun_finished:', stun_client.A().stun_results.values()
-        min_port = 999999
-        max_port = 0
-        if len(sys.argv) > 3:
-            min_port = int(sys.argv[2])
-            max_port = int(sys.argv[3])
-        else:
-            for addr, port in stun_client.A().stun_results.values():
-                print addr, port
-                if port > max_port:
-                    max_port = port
-                if port < min_port:
-                    min_port = port
-        print 'ports range:', min_port, max_port
-        loop_send_packets(min_port, max_port)
-    stun_client.A('start', (int(settings.getUDPPort()), _finished))
+    
+    if sys.argv[1] == 'server':
+        min_port = int(sys.argv[2])
+        max_port = int(sys.argv[3])
+        for port_num in range(min_port, max_port+1):
+            udp.listen(port_num)
+            udp.proto(port_num).add_callback(lambda d,a: datagram_received(d, a, port_num))
+            
+    elif sys.argv[1] == 'listen':
+        port_num = int(sys.argv[2])
+        udp.listen(port_num)
+        udp.proto(port_num).add_callback(lambda d,a: datagram_received(d, a, port_num))
+        servers = []
+        for line in open(sys.argv[3]).read().split('\n'):
+            addr = line.split(':')
+            addr[1] = int(addr[1])
+            servers.append(addr)
+        listen(port_num, servers, sys.argv[4])
+        
+    elif sys.argv[1] == 'connect':
+        port_num = int(sys.argv[2])
+        remote_ip = sys.argv[3]
+        udp.listen(port_num)
+        udp.proto(port_num).add_callback(lambda d,a: datagram_received(d, a, port_num))
+        servers = []
+        for line in open(sys.argv[4]).read().split('\n'):
+            addr = line.split(':')
+            addr[1] = int(addr[1])
+            servers.append(addr)
+        min_port = int(sys.argv[5])
+        max_port = int(sys.argv[6])
+        connect(port_num, remote_ip, servers, min_port, max_port)
+        
     reactor.run()
 
 if __name__ == '__main__':
