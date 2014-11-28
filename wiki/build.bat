73a167115aaa0b3843ee56cde2793410821ba2cd
Veselin <penev.veselin@gmail.com>
2014-11-28 23:41:21 +0400

    working on network services tree visualisation

--- wiki/build.bat
+++ wiki/build.bat
@@ -1,4 +1,5 @@
 @echo off
-python build_area.py
+python build_area.py ../services/html_src/services_png_1.htm
 python -c "s=open('map.template').read();s=s.replace('{area}',open('area').read());open('map.html','w').write(s);"
+rm -rf ../services/html_src/*
 pause
--- wiki/build_area.py
+++ wiki/build_area.py
@@ -1,5 +1,7 @@
-import re;
-s=open('src/services_png_1.htm').read();
+import re,sys;
+s=open(sys.argv[1]).read();
 m=s[s.find('<MAP NAME=\"visImageMap\">'):s.find('</MAP>')+6];
-m=re.sub('on\w+?=\".+?\"', '', m)
+m=re.sub('on\w+?=\".+?\"','',m);
+m=re.sub('%5C','/',m);
+m=re.sub('.vsd','.png',m);
 open('area','w').write(m);
