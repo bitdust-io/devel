aea04acf9f2b26ab5cf64f7400c5b6817d937fc2
Veselin <penev.veselin@gmail.com>
2014-12-03 22:58:46 +0400

    docs

--- services/build_map.bat
+++ services/build_map.bat
@@ -1,5 +1,6 @@
 @echo off
-python build_area.py ../services/html_src/services_png_1.htm
+python build_area.py html_src/services_png_1.htm
 python -c "s=open('map.template').read();s=s.replace('{area}',open('area').read());open('map.html','w').write(s);"
-rm -rf ../services/html_src/*
+mv html_src/services_png_1.png services.png 
+rm -rf html_src/*
 pause
