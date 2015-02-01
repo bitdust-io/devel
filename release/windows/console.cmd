7100e67611bf1619eefc09bb886a8785201dd0e5
Veselin <penev.veselin@gmail.com>
2015-02-02 00:26:48 +0400

    building a Windows release

---
+++ release/windows/console.cmd
@@ -0,0 +1,1 @@
+START cmd /k "cd %USERPROFILE%\.bitdust\bin\ & echo Welcome to BitDust project! & echo Type `bitdust usage` to list all available commands and options."
---
+++ release/windows/run.cmd
@@ -0,0 +1,3 @@
+@echo off
+cd %USERPROFILE%\.bitdust\bin\
+bitdust.exe
