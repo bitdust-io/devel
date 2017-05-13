8689c76e35f46104796c073f393d4107eb92a98f
Veselin Penev <veselin.penev@kpn.com>
2017-05-13 20:07:36 +0200

    modified:   dht/known_nodes.py

--- dht/known_nodes.py
+++ dht/known_nodes.py
@@ -38,12 +38,12 @@ def nodes():
     Here is a well known DHT nodes, this is "genesis" network.
     Every new node in the network will first connect one or several of those nodes,
     and then will be routed to some other nodes already registered.
-    
+
     Right now we have started several BitDust nodes on vps hostsing across the world.
     If you willing to support the project and already started your own BitDust node on reliable machine,
     contact us and we will include your address here.
     So other nodes will be able to use your machine to connect to DHT network.
-    
+
     The load is not big and as network will grow we will have more machines listed here,
     so all traffic will be distributed accross the web.
     """
@@ -54,4 +54,5 @@ def nodes():
         ('86.110.117.159', 14441), # veselin-p2p.ru
         ('185.65.200.231', 14441), # bitdust.io
         ('45.32.246.95', 14441),   # bitdust.ai
+        ('209.59.119.33', 14441),  # work.offshore.ai
     ]
