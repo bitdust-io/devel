be779dcdc7caba4371f649e72709323995d95907
Veselin Penev <penev.veselin@gmail.com>
2018-03-08 23:18:24 +0100

    updated storage/restore.py

--- storage/backup.py
+++ storage/backup.py
@@ -172,7 +172,7 @@ class backup(automat.Automat):
         self.totalSize = -1
         self.finishCallback = finishCallback
         self.blockResultCallback = blockResultCallback
-        automat.Automat.__init__(self, 'backup_%s' % self.backupID, 'AT_STARTUP', _DebugLevel)
+        automat.Automat.__init__(self, 'backup_%s' % self.version, 'AT_STARTUP', _DebugLevel)
 
     #------------------------------------------------------------------------------
 
