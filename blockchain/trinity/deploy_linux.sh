a27f0286f1b95d986b739c6a367674273ce73012
Veselin Penev <penev.veselin@gmail.com>
2018-07-18 18:09:07 +0200

    modified:   blockchain/trinity/deploy_linux.sh

--- blockchain/trinity/deploy_linux.sh
+++ blockchain/trinity/deploy_linux.sh
@@ -1,10 +1,13 @@
 #/bin/sh
 
 ROOT_DIR="$HOME/.bitdust"
-TRINITY_DIR="${ROOT_DIR}/trinity"
-TRINITY_VENV="${TRINITY_DIR}/venv"
-TRINITY_BIN="${TRINITY_DIR}/bin/"
-TRINITY_VIRTUALENV="${TRINITY_DIR}/bin/virtualenv"
+BLOCKCHAIN_DIR="${ROOT_DIR}/blockchain"
+BLOCKCHAIN_SRC="${BLOCKCHAIN_DIR}/src"
+BLOCKCHAIN_DATA="${BLOCKCHAIN_DIR}/data"
+BLOCKCHAIN_VENV="${BLOCKCHAIN_SRC}/venv"
+BLOCKCHAIN_PYTHON="${BLOCKCHAIN_VENV}/bin/python3.6"
+BLOCKCHAIN_VIRTUALENV="${BLOCKCHAIN_DIR}/virtualenv"
+BLOCKCHAIN_VIRTUALENV_BIN="${BLOCKCHAIN_VIRTUALENV}/bin/virtualenv"
 
 
 if ! [ -x "$(command -v python3.6)" ]; then
@@ -37,53 +40,69 @@ else
 fi
 
 
-if [ ! -d $TRINITY_DIR ]; then
-    mkdir -p $TRINITY_DIR
-    mkdir $TRINITY_DIR/bin
-    mkdir $TRINITY_DIR/data
-    mkdir $TRINITY_DIR/data/logs
-    echo '' > $TRINITY_DIR/data/logs/trinity.log
-    echo ''
-    echo "##### Created folder for Trinity Blockchain in ${TRINITY_DIR}"
-fi
-
-
-if ! [ -f $TRINITY_VIRTUALENV/bin/virtualenv ]; then
+if ! [ -f $BLOCKCHAIN_VIRTUALENV/bin/virtualenv ]; then
     echo ''
     echo '##### Installing isolated virtualenv'
-    PYTHONUSERBASE=$TRINITY_VIRTUALENV pip3 install --ignore-installed --user virtualenv
+    PYTHONUSERBASE=$BLOCKCHAIN_VIRTUALENV pip3 install --ignore-installed --user virtualenv
 else
     echo ''
     echo '##### Found isolated virtualenv binaries'
 fi
 
 
-if [ ! -d $TRINITY_VENV ]; then
+if [ ! -d $BLOCKCHAIN_DIR ]; then
+    mkdir -p $BLOCKCHAIN_DIR
+    mkdir -p $BLOCKCHAIN_DATA
+    mkdir -p $BLOCKCHAIN_DATA/logs
+    mkdir -p $BLOCKCHAIN_VIRTUALENV
+    echo '' > $BLOCKCHAIN_DATA/logs/trinity.log
     echo ''
-    echo '##### Building Trinity virtual environment'
-    PYTHONUSERBASE=$TRINITY_VIRTUALENV $TRINITY_VIRTUALENV/bin/virtualenv -p python3.6 $TRINITY_VENV
+    echo "##### Created required folders for Py-EVM Blockchain in ${BLOCKCHAIN_DIR}"
 fi
 
 
-if [ ! -f $TRINITY_VENV/bin/pip ]; then
+if ! [ -f $BLOCKCHAIN_SRC/setup.py ]; then
+    echo ''
+    echo '##### Cloning Py-EVM repository'
+    git clone --depth=1 https://github.com/vesellov/py-evm.git $BLOCKCHAIN_SRC
+else
+    echo ''
+    echo '##### Updating the source code of Py-EVM'
+    cd $BLOCKCHAIN_SRC
+    git fetch
+    git reset --hard origin/master
+    cd $OLDPWD
+fi
+
+
+if [ ! -d $BLOCKCHAIN_VENV ]; then
+    echo ''
+    echo '##### Building Py-EVM virtual environment'
+    PYTHONUSERBASE=$BLOCKCHAIN_VIRTUALENV $BLOCKCHAIN_VIRTUALENV_BIN -p python3.6 $BLOCKCHAIN_VENV
+fi
+
+
+if [ ! -f $BLOCKCHAIN_VENV/bin/pip ]; then
     echo ''
     echo '##### Pip is not found inside virtual environment, rebuilding'
-    rm -rf $TRINITY_VENV
-    PYTHONUSERBASE=$TRINITY_VIRTUALENV $TRINITY_VIRTUALENV/bin/virtualenv -p python3.6 $TRINITY_VENV
+    rm -rf $BLOCKCHAIN_VENV
+    PYTHONUSERBASE=$BLOCKCHAIN_VIRTUALENV $BLOCKCHAIN_VIRTUALENV_BIN -p python3.6 $BLOCKCHAIN_VENV
 fi
 
 
 echo ''
 echo '##### Installing/Updating trinity with pip'
-echo ''
-$TRINITY_VENV/bin/pip install -U trinity
+cd $BLOCKCHAIN_SRC
+$BLOCKCHAIN_PYTHON setup.py install
 
 
 echo ''
 echo 'To start trinity process you can run via command line:'
-echo "${TRINITY_VENV}/bin/trinity --data-dir=${TRINITY_DIR}/data --trinity-root-dir=${TRINITY_DIR} --port=30345"
+echo ''
+echo "${BLOCKCHAIN_VENV}/bin/trinity --data-dir=${BLOCKCHAIN_DATA} --port=30345"
 
 
+echo ''
 echo ''
 echo 'DONE!'
 echo ''
