1cff0a2e846801393cd1fa56f262374442b58cfd
Veselin Penev <penev.veselin@gmail.com>
2018-07-27 10:40:37 +0200

    isolated SSL dependecies in old/unused methods in lib.net_misc

--- lib/net_misc.py
+++ lib/net_misc.py
@@ -52,7 +52,6 @@ except:
     sys.exit('Error initializing twisted.internet.reactor in net_misc.py')
 
 from twisted.internet.defer import Deferred, DeferredList, succeed, fail
-from twisted.internet import ssl
 from twisted.internet import protocol
 from twisted.python.failure import Failure
 # from twisted.internet.utils import getProcessOutput
@@ -64,8 +63,6 @@ from twisted.web.client import downloadPage
 from twisted.web.client import HTTPDownloader
 
 from zope.interface import implements
-
-from OpenSSL import SSL
 
 #------------------------------------------------------------------------------
 
@@ -350,6 +347,7 @@ def downloadWithProgressTwisted(url, file, progress_func):
     This method can keep track of the progress.
     """
     global _UserAgentString
+    from twisted.internet import ssl
     scheme, host, port, path = parse_url(url)
     factory = HTTPProgressDownloader(url, file, progress_func, agent=_UserAgentString)
     if scheme == 'https':
@@ -367,6 +365,7 @@ def downloadSSLWithProgressTwisted(url, file, progress_func, privateKeyFileName,
     Can download from HTTPS sites.
     """
     global _UserAgentString
+    from twisted.internet import ssl
     scheme, host, port, path = parse_url(url)
     factory = HTTPProgressDownloader(url, file, progress_func, agent=_UserAgentString)
     if scheme != 'https':
@@ -378,33 +377,16 @@ def downloadSSLWithProgressTwisted(url, file, progress_func, privateKeyFileName,
 
 #-------------------------------------------------------------------------------
 
-
-class MyClientContextFactory(ssl.ClientContextFactory):
-
-    def __init__(self, certificates_filenames):
-        self.certificates_filenames = list(certificates_filenames)
-
-    def verify(self, connection, x509, errnum, errdepth, ok):
-        return ok
-
-    def getContext(self):
-        ctx = ssl.ClientContextFactory.getContext(self)
-        for cert in self.certificates_filenames:
-            try:
-                ctx.load_verify_locations(cert)
-            except:
-                pass
-        ctx.set_verify(SSL.VERIFY_PEER | SSL.VERIFY_FAIL_IF_NO_PEER_CERT, self.verify)
-        return ctx
-
-
 def downloadSSL(url, fileOrName, progress_func, certificates_filenames):
     """
     Another method to download from HTTPS.
     """
     global _UserAgentString
+    from twisted.internet import ssl
+    from OpenSSL import SSL
+
     scheme, host, port, path = parse_url(url)
-    if not isinstance(certificates_filenames, types.ListType):
+    if not isinstance(certificates_filenames, list):
         certificates_filenames = [certificates_filenames, ]
     cert_found = False
     for cert in certificates_filenames:
@@ -413,6 +395,25 @@ def downloadSSL(url, fileOrName, progress_func, certificates_filenames):
             break
     if not cert_found:
         return fail(Exception('no one ssl certificate found'))
+        
+    class MyClientContextFactory(ssl.ClientContextFactory):
+    
+        def __init__(self, certificates_filenames):
+            self.certificates_filenames = list(certificates_filenames)
+    
+        def verify(self, connection, x509, errnum, errdepth, ok):
+            return ok
+    
+        def getContext(self):
+            ctx = ssl.ClientContextFactory.getContext(self)
+            for cert in self.certificates_filenames:
+                try:
+                    ctx.load_verify_locations(cert)
+                except:
+                    pass
+            ctx.set_verify(SSL.VERIFY_PEER | SSL.VERIFY_FAIL_IF_NO_PEER_CERT, self.verify)
+            return ctx
+    
     factory = HTTPDownloader(url, fileOrName, agent=_UserAgentString)
     contextFactory = MyClientContextFactory(certificates_filenames)
     reactor.connectSSL(host, port, factory, contextFactory)
@@ -706,201 +707,6 @@ def SendEmail(TO, FROM, HOST, PORT, LOGIN, PASSWORD, SUBJECT, BODY, FILES):
 
 #-------------------------------------------------------------------------------
 
-# Great Thanks to Mariano!
-# http://marianoiglesias.com.ar/python/file-uploading-with-multi-part-encoding-using-twisted/
-
-class StringReceiver(protocol.Protocol):
-    buffer = ""
-
-    def __init__(self, deferred=None):
-        self._deferred = deferred
-
-    def dataReceived(self, data):
-        self.buffer += data
-
-    def connectionLost(self, reason):
-        if self._deferred and reason.check(client.ResponseDone):
-            self._deferred.callback(self.buffer)
-        else:
-            self._deferred.errback(Exception(self.buffer))
-
-
-class MultiPartProducer:
-    implements(iweb.IBodyProducer)
-    CHUNK_SIZE = 2 ** 8
-
-    def __init__(self, files={}, data={}, callback=None, deferred=None):
-        self._files = files
-        self._file_lengths = {}
-        self._data = data
-        self._callback = callback
-        self._deferred = deferred
-        self.boundary = self._boundary()
-        self.length = self._length()
-
-    def startProducing(self, consumer):
-        self._consumer = consumer
-        self._current_deferred = Deferred()
-        self._sent = 0
-        self._paused = False
-        if not hasattr(self, "_chunk_headers"):
-            self._build_chunk_headers()
-        if self._data:
-            block = ""
-            for field in self._data:
-                block += self._chunk_headers[field]
-                block += self._data[field]
-                block += "\r\n"
-            self._send_to_consumer(block)
-        if self._files:
-            self._files_iterator = self._files.iterkeys()
-            self._files_sent = 0
-            self._files_length = len(self._files)
-            self._current_file_path = None
-            self._current_file_handle = None
-            self._current_file_length = None
-            self._current_file_sent = 0
-            result = self._produce()
-            if result:
-                return result
-        else:
-            return succeed(None)
-        return self._current_deferred
-
-    def resumeProducing(self):
-        self._paused = False
-        result = self._produce()
-        if result:
-            return result
-
-    def pauseProducing(self):
-        self._paused = True
-
-    def stopProducing(self):
-        self._finish(True)
-        if self._deferred and self._sent < self.length:
-            self._deferred.errback(Exception("Consumer asked to stop production of request body (%d sent out of %d)" % (self._sent, self.length)))
-
-    def _produce(self):
-        if self._paused:
-            return
-        done = False
-        while not done and not self._paused:
-            if not self._current_file_handle:
-                field = self._files_iterator.next()
-                self._current_file_path = self._files[field]
-                self._current_file_sent = 0
-                self._current_file_length = self._file_lengths[field]
-                self._current_file_handle = open(self._current_file_path, "rb")
-                self._send_to_consumer(self._chunk_headers[field])
-            chunk = self._current_file_handle.read(self.CHUNK_SIZE)
-            if chunk:
-                self._send_to_consumer(chunk)
-                self._current_file_sent += len(chunk)
-            if not chunk or self._current_file_sent == self._current_file_length:
-                self._send_to_consumer("\r\n")
-                self._current_file_handle.close()
-                self._current_file_handle = None
-                self._current_file_sent = 0
-                self._current_file_path = None
-                self._files_sent += 1
-            if self._files_sent == self._files_length:
-                done = True
-        if done:
-            self._send_to_consumer("--%s--\r\n" % self.boundary)
-            self._finish()
-            return succeed(None)
-
-    def _finish(self, forced=False):
-        if hasattr(self, "_current_file_handle") and self._current_file_handle:
-            self._current_file_handle.close()
-        if self._current_deferred:
-            self._current_deferred.callback(self._sent)
-            self._current_deferred = None
-        if not forced and self._deferred:
-            self._deferred.callback(self._sent)
-
-    def _send_to_consumer(self, block):
-        self._consumer.write(block)
-        self._sent += len(block)
-        if self._callback:
-            self._callback(self._sent, self.length)
-
-    def _length(self):
-        self._build_chunk_headers()
-        length = 0
-        if self._data:
-            for field in self._data:
-                length += len(self._chunk_headers[field])
-                length += len(self._data[field])
-                length += 2
-        if self._files:
-            for field in self._files:
-                length += len(self._chunk_headers[field])
-                length += self._file_size(field)
-                length += 2
-        length += len(self.boundary)
-        length += 6
-        return length
-
-    def _build_chunk_headers(self):
-        if hasattr(self, "_chunk_headers") and self._chunk_headers:
-            return
-        self._chunk_headers = {}
-        for field in self._files:
-            self._chunk_headers[field] = self._headers(field, True)
-        for field in self._data:
-            self._chunk_headers[field] = self._headers(field)
-
-    def _headers(self, name, is_file=False):
-        value = self._files[name] if is_file else self._data[name]
-        _boundary = self.boundary.encode("utf-8") if isinstance(self.boundary, unicode) else urllib.quote_plus(self.boundary)
-        headers = ["--%s" % _boundary]
-        if is_file:
-            disposition = 'form-data; name="%s"; filename="%s"' % (name, os.path.basename(value))
-        else:
-            disposition = 'form-data; name="%s"' % name
-        headers.append("Content-Disposition: %s" % disposition)
-        if is_file:
-            file_type = self._file_type(name)
-        else:
-            file_type = "text/plain; charset=utf-8"
-        headers.append("Content-Type: %s" % file_type)
-        if is_file:
-            headers.append("Content-Length: %i" % self._file_size(name))
-        else:
-            headers.append("Content-Length: %i" % len(value))
-        headers.append("")
-        headers.append("")
-        return "\r\n".join(headers)
-
-    def _boundary(self):
-        boundary = None
-        try:
-            import uuid
-            boundary = uuid.uuid4().hex
-        except ImportError:
-            import random
-            import sha
-            bits = random.getrandbits(160)
-            boundary = sha.new(str(bits)).hexdigest()
-        return boundary
-
-    def _file_type(self, field):
-        type = mimetypes.guess_type(self._files[field])[0]
-        return type.encode("utf-8") if isinstance(type, unicode) else str(type)
-
-    def _file_size(self, field):
-        size = 0
-        try:
-            handle = open(self._files[field], "r")
-            size = os.fstat(handle.fileno()).st_size
-            handle.close()
-        except:
-            size = 0
-        self._file_lengths[field] = size
-        return self._file_lengths[field]
-
 #------------------------------------------------------------------------------
 
 
@@ -908,10 +714,205 @@ def uploadHTTP(url, files, data, progress=None, receiverDeferred=None):
     """
     A smart way to upload a file over HTTP POST method.
 
-    Use ``MultiPartProducer`` and ``StringReceiver`` classes. Great
-    Thanks to Mariano!     http://marianoiglesias.com.ar/python/file-
-    uploading-with-multi-part-encoding-using-twisted/
+    Use ``MultiPartProducer`` and ``StringReceiver`` classes.
+    Great Thanks to Mariano!
+
+    http://marianoiglesias.com.ar/python/file-uploading-with-multi-part-encoding-using-twisted/
     """
+    
+    class StringReceiver(protocol.Protocol):
+        buffer = ""
+    
+        def __init__(self, deferred=None):
+            self._deferred = deferred
+    
+        def dataReceived(self, data):
+            self.buffer += data
+    
+        def connectionLost(self, reason):
+            if self._deferred and reason.check(client.ResponseDone):
+                self._deferred.callback(self.buffer)
+            else:
+                self._deferred.errback(Exception(self.buffer))
+    
+    
+    class MultiPartProducer:
+        implements(iweb.IBodyProducer)
+        CHUNK_SIZE = 2 ** 8
+    
+        def __init__(self, files={}, data={}, callback=None, deferred=None):
+            self._files = files
+            self._file_lengths = {}
+            self._data = data
+            self._callback = callback
+            self._deferred = deferred
+            self.boundary = self._boundary()
+            self.length = self._length()
+    
+        def startProducing(self, consumer):
+            self._consumer = consumer
+            self._current_deferred = Deferred()
+            self._sent = 0
+            self._paused = False
+            if not hasattr(self, "_chunk_headers"):
+                self._build_chunk_headers()
+            if self._data:
+                block = ""
+                for field in self._data:
+                    block += self._chunk_headers[field]
+                    block += self._data[field]
+                    block += "\r\n"
+                self._send_to_consumer(block)
+            if self._files:
+                self._files_iterator = self._files.iterkeys()
+                self._files_sent = 0
+                self._files_length = len(self._files)
+                self._current_file_path = None
+                self._current_file_handle = None
+                self._current_file_length = None
+                self._current_file_sent = 0
+                result = self._produce()
+                if result:
+                    return result
+            else:
+                return succeed(None)
+            return self._current_deferred
+    
+        def resumeProducing(self):
+            self._paused = False
+            result = self._produce()
+            if result:
+                return result
+    
+        def pauseProducing(self):
+            self._paused = True
+    
+        def stopProducing(self):
+            self._finish(True)
+            if self._deferred and self._sent < self.length:
+                self._deferred.errback(Exception("Consumer asked to stop production of request body (%d sent out of %d)" % (self._sent, self.length)))
+    
+        def _produce(self):
+            if self._paused:
+                return
+            done = False
+            while not done and not self._paused:
+                if not self._current_file_handle:
+                    field = self._files_iterator.next()
+                    self._current_file_path = self._files[field]
+                    self._current_file_sent = 0
+                    self._current_file_length = self._file_lengths[field]
+                    self._current_file_handle = open(self._current_file_path, "rb")
+                    self._send_to_consumer(self._chunk_headers[field])
+                chunk = self._current_file_handle.read(self.CHUNK_SIZE)
+                if chunk:
+                    self._send_to_consumer(chunk)
+                    self._current_file_sent += len(chunk)
+                if not chunk or self._current_file_sent == self._current_file_length:
+                    self._send_to_consumer("\r\n")
+                    self._current_file_handle.close()
+                    self._current_file_handle = None
+                    self._current_file_sent = 0
+                    self._current_file_path = None
+                    self._files_sent += 1
+                if self._files_sent == self._files_length:
+                    done = True
+            if done:
+                self._send_to_consumer("--%s--\r\n" % self.boundary)
+                self._finish()
+                return succeed(None)
+    
+        def _finish(self, forced=False):
+            if hasattr(self, "_current_file_handle") and self._current_file_handle:
+                self._current_file_handle.close()
+            if self._current_deferred:
+                self._current_deferred.callback(self._sent)
+                self._current_deferred = None
+            if not forced and self._deferred:
+                self._deferred.callback(self._sent)
+    
+        def _send_to_consumer(self, block):
+            self._consumer.write(block)
+            self._sent += len(block)
+            if self._callback:
+                self._callback(self._sent, self.length)
+    
+        def _length(self):
+            self._build_chunk_headers()
+            length = 0
+            if self._data:
+                for field in self._data:
+                    length += len(self._chunk_headers[field])
+                    length += len(self._data[field])
+                    length += 2
+            if self._files:
+                for field in self._files:
+                    length += len(self._chunk_headers[field])
+                    length += self._file_size(field)
+                    length += 2
+            length += len(self.boundary)
+            length += 6
+            return length
+    
+        def _build_chunk_headers(self):
+            if hasattr(self, "_chunk_headers") and self._chunk_headers:
+                return
+            self._chunk_headers = {}
+            for field in self._files:
+                self._chunk_headers[field] = self._headers(field, True)
+            for field in self._data:
+                self._chunk_headers[field] = self._headers(field)
+    
+        def _headers(self, name, is_file=False):
+            value = self._files[name] if is_file else self._data[name]
+            _boundary = self.boundary.encode("utf-8") if isinstance(self.boundary, unicode) else urllib.quote_plus(self.boundary)
+            headers = ["--%s" % _boundary]
+            if is_file:
+                disposition = 'form-data; name="%s"; filename="%s"' % (name, os.path.basename(value))
+            else:
+                disposition = 'form-data; name="%s"' % name
+            headers.append("Content-Disposition: %s" % disposition)
+            if is_file:
+                file_type = self._file_type(name)
+            else:
+                file_type = "text/plain; charset=utf-8"
+            headers.append("Content-Type: %s" % file_type)
+            if is_file:
+                headers.append("Content-Length: %i" % self._file_size(name))
+            else:
+                headers.append("Content-Length: %i" % len(value))
+            headers.append("")
+            headers.append("")
+            return "\r\n".join(headers)
+    
+        def _boundary(self):
+            boundary = None
+            try:
+                import uuid
+                boundary = uuid.uuid4().hex
+            except ImportError:
+                import random
+                import sha
+                bits = random.getrandbits(160)
+                boundary = sha.new(str(bits)).hexdigest()
+            return boundary
+    
+        def _file_type(self, field):
+            type = mimetypes.guess_type(self._files[field])[0]
+            return type.encode("utf-8") if isinstance(type, unicode) else str(type)
+    
+        def _file_size(self, field):
+            size = 0
+            try:
+                handle = open(self._files[field], "r")
+                size = os.fstat(handle.fileno()).st_size
+                handle.close()
+            except:
+                size = 0
+            self._file_lengths[field] = size
+            return self._file_lengths[field]
+    
+
     # producerDeferred = Deferred()
     receiverDeferred = Deferred()
 
