c0d03fb3dbbd2695eb37e34b4b7205b4bd861ff9
Veselin Penev <penev.veselin@gmail.com>
2018-12-26 21:21:35 +0100

    fixed regression/tests/services/test_service_customer.py

--- regression/tests/services/test_service_customer.py
+++ regression/tests/services/test_service_customer.py
@@ -8,14 +8,13 @@ def test_customer_1_enough_suppliers():
     count = 0
     while True:
         if count > 10:
-            assert True, 'customer failed to hire enough suppliers for too long, but lets pass the test...  TODO:'
+            assert False, 'customer failed to hire enough suppliers after many attempts'
             return 
         response = requests.get(url=tunnel_url('customer_1', 'supplier/list/v1'))
         assert response.status_code == 200
         print('supplier/list/v1 response: %r' % response.json())
         assert response.json()['status'] == 'OK', response.json()
-        # assert response.json()['result'] == 2, 'still wait 2 suppliers'
-        if response.json()['result'] == 2:
+        if len(response.json()['result']) == 2:
             print('OK found 2 suppliers in response')
             assert True
             break
