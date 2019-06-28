a16685a1d39179e2802f05abbbd809b5da863ce0
Veselin Penev <penev.veselin@gmail.com>
2019-06-29 00:23:46 +0200

    try to set DHT backet size to 8

--- p2p/p2p_queue.py
+++ p2p/p2p_queue.py
@@ -696,7 +696,8 @@ def do_consume(interested_consumers=None):
         interested_queues = set()
         for queue_id in consumer(consumer_id).queues:
             if queue_id not in queue():
-                lg.warn('consumer queue %s was not found' % queue_id)
+                if _Debug:
+                    lg.out(_DebugLevel, 'p2p_queue.do_consume consumer queue %s was not found' % queue_id)
                 continue
             if len(queue(queue_id)) == 0:
                 # no messages in the queue
