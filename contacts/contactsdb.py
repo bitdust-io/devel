5cb647ca2611dbdb5d2c3a53504b88b068396853
Veselin Penev <vesellov@gmail.com>
2019-11-23 23:30:13 +0100

    bump twisted to 19.10.0 in requirements.txt  (#451)
    * bump twisted to 19.10.0 in requirements.txt
    
    * updated CHANGELOG, prepare next release

--- contacts/contactsdb.py
+++ contacts/contactsdb.py
@@ -800,7 +800,6 @@ def load_contacts():
     Load all my contacts from disk.
     """
     load_suppliers(all_customers=True)
-    # load_suppliers()
     if _SuppliersChangedCallback is not None:
         _SuppliersChangedCallback([], suppliers())
     load_customers()
