db709459d8d81ba2835d08b0e4f6bbca5df8e2ff
Veselin Penev <penev.veselin@gmail.com>
2020-01-25 11:15:04 +0100

    bug fix

--- regress/fetch_logs.sh
+++ regress/fetch_logs.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-PYTHON2=`which python`
-PYTHON3=`which python3`
-
-
-if [ -z "$PYTHON2" ] then
-    PYTHON="$PYTHON2"
-else
-    PYTHON="$PYTHON3"
-fi
-
-$PYTHON --version
-
-exit 0
-
-
 test_name="$1"
 
 rm -rf logs/$test_name
@@ -23,16 +8,20 @@ mkdir logs/$test_name
 allnodesfile="allnodes"
 rm -rf $allnodesfile
 
-echo "$test_name" | $PYTHON -c "import json,sys; tst=sys.stdin.read().strip(); containers=json.loads(open('tests/%s/conf.json' % tst).read())['containers'].keys(); open('allnodes', 'w').write(' '.join(containers));"
+echo "$test_name" | python -c "import json,sys; tst=sys.stdin.read().strip(); containers=json.loads(open('tests/%s/conf.json' % tst).read())['containers'].keys(); open('allnodes', 'w').write(' '.join(containers));"
 
 nodes=`cat $allnodesfile`
 
 for node in $nodes; do
     echo "[$node]";
-    docker-compose --file tests/$test_name/docker-compose.yml exec -T $node sh -c "cat /root/.bitdust/logs/automats.log" > logs/$test_name/state.$node.log;
-    docker-compose --file tests/$test_name/docker-compose.yml exec -T $node sh -c "cat /root/.bitdust/logs/event.log" > logs/$test_name/event.$node.log;
-    docker-compose --file tests/$test_name/docker-compose.yml exec -T $node sh -c "cat /root/.bitdust/logs/packet.log" > logs/$test_name/packet.$node.log;
-    docker-compose --file tests/$test_name/docker-compose.yml exec -T $node sh -c "cat /root/.bitdust/logs/stdout.log" > logs/$test_name/stdout.$node.log;
+    docker-compose --file tests/$test_name/docker-compose.yml exec -T $node sh -c "cat /root/.bitdust/logs/automats.log" 1> logs/$test_name/state.$node.log 2>/dev/null;
+    docker-compose --file tests/$test_name/docker-compose.yml exec -T $node sh -c "cat /root/.bitdust/logs/event.log" 1> logs/$test_name/event.$node.log 2>/dev/null;
+    docker-compose --file tests/$test_name/docker-compose.yml exec -T $node sh -c "cat /root/.bitdust/logs/packet.log" 1> logs/$test_name/packet.$node.log 2>/dev/null;
+    docker-compose --file tests/$test_name/docker-compose.yml exec -T $node sh -c "cat /root/.bitdust/logs/stdout.log" 1> logs/$test_name/stdout.$node.log 2>/dev/null;
+    docker-compose --file tests/$test_name/docker-compose.yml exec -T $node sh -c "cat /root/.bitdust/logs/err.log" 1> logs/$test_name/err.$node.log 2>/dev/null;
+    docker-compose --file tests/$test_name/docker-compose.yml exec -T $node sh -c "cat /root/.bitdust/logs/main.log" 1> logs/$test_name/main.$node.log 2>/dev/null;
+    docker-compose --file tests/$test_name/docker-compose.yml exec -T $node sh -c "cat /root/.bitdust/logs/warn.log" 1> logs/$test_name/warn.$node.log 2>/dev/null;
+    docker-compose --file tests/$test_name/docker-compose.yml exec -T $node sh -c "cat /root/.bitdust/logs/exception_*.log" 1> logs/$test_name/exception.$node.log 2>/dev/null;
 done
 
 rm -rf $allnodesfile
