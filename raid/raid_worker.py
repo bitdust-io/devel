666a7e7825cbe20d85a9df7a0d392e69d83afc9b
Veselin Penev <penev.veselin@gmail.com>
2020-08-25 08:17:55 +0200

    fix in   raid/raid_worker.py

--- raid/raid_worker.py
+++ raid/raid_worker.py
@@ -500,6 +500,7 @@ class ThreadedRaidProcessor(object):
     def cancel(self, task_id):
         if task_id not in self.active_tasks:
             lg.warn('can not cancel task %r, task was not found' % task_id)
+            return
         self.active_tasks[task_id].stop()
 
     def destroy(self):
